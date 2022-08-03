a28adbd5fa3071969a605edc57f7a57d685ff295
judsonpaiva <judson.paiva@hotmail.com>
2022-08-03 21:21:57 +0100

    retorno do dia de semana

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,33 @@ const h1 = document.querySelector(".container h1");
 const data = new Date();
 
 h1.innerHTML = data.toISOString();
+
+
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 1:
+            diaSemanaTexto = 'Domingo';
+            break;
+        case 2:
+            diaSemanaTexto = 'Segunda-Feira';
+            break;
+        case 3:
+            diaSemanaTexto = 'Terça-Feira';
+            break;
+        case 4:
+            diaSemanaTexto = 'Quarta-Feira';
+            break;
+        case 5:
+            diaSemanaTexto = 'Quinta-Feira';
+            break;
+        case 6:
+            diaSemanaTexto = 'Sexta-Feira';
+            break;
+        case 7:
+            diaSemanaTexto = 'Sábado';
+            break;
+    }
+    return diaSemanaTexto;
+}
